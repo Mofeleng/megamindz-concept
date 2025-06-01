bcd0b417c74857f893e7526bfb79f34f91341239
Mofeleng <mofelengmokgope@gmail.com>
2025-06-01 20:03:38 +0200

    Updated fetch options

--- src/lib/fetch-options.ts
+++ src/lib/fetch-options.ts
@@ -1,10 +1,9 @@
-
-export function returnFetchOptions (data: {}) {
-    return {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    }
+export function returnFetchOptions<T extends Record<string, any>>(data: T): RequestInit {
+  return {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
 }
